3fdf9c7c919f3965f67624755f8a96df2e44db33
Gáll Zoltán <gallzoltan@gmail.com>
2022-04-24 16:22:05 +0200

    add chart, and present all data on chart

--- src/app.js
+++ src/app.js
@@ -3,13 +3,40 @@ import './scss/loader.scss'
 import covidApi from './services/covidApi';
 import ReportCard from './components/ReportCard.js'
 import CountryList from './components/CountryList';
+import Chart from 'chart.js/auto';
 
 const loader = document.querySelector("#loading");
 
 window.addEventListener('DOMContentLoaded', () => {   
-  loadComponents(); 
+  registerComponents(); 
   const form = document.getElementById('form'); 
-  const submitButton = document.getElementById('submit');
+  const submitButton = document.getElementById('submit');  
+  
+  // const labels = [
+  //   'January',
+  //   'February',
+  //   'March',
+  //   'April',
+  //   'May',
+  //   'June',
+  // ];
+
+  // const data = {
+  //   labels: labels,
+  //   datasets: [{
+  //     label: 'My First dataset',
+  //     backgroundColor: 'rgb(255, 99, 132)',
+  //     borderColor: 'rgb(255, 99, 132)',
+  //     data: [0, 10, 5, 2, 20, 30, 45],
+  //   }]
+  // };
+
+  // const config = {
+  //   type: 'line',
+  //   data: data,
+  //   options: {}
+  // };
+  // new Chart(document.getElementById('axes_line_chart'), config);
   displayLoading();    
   renderCurrentDatas('HU'); 
 
@@ -23,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }); 
 });
 
-const loadComponents = () => {
+const registerComponents = () => {
   customElements.define("report-card", ReportCard);
   customElements.define("country-list", CountryList);
 }
@@ -32,19 +59,25 @@ const renderCurrentDatas = (ab) => {
   const cardConfirmed = document.getElementById('confirmed');
   const cardRecovered = document.getElementById('recovered');
   const cardDeaths= document.getElementById('deaths');
+  const ctx = document.getElementById('axes_line_chart').getContext("2d");
   loadCurrentData(ab).then(res => {
     const local = Intl.NumberFormat('hu-HU');
     cardConfirmed.setDatas({current: local.format(res.confirmed), update: res.updated});
     cardRecovered.setDatas({current: local.format(res.recovered), update: res.updated});
-    cardDeaths.setDatas({current: local.format(res.deaths), update: res.updated});
-    hideLoading();
-  });
+    cardDeaths.setDatas({current: local.format(res.deaths), update: res.updated});    
+
+    loadHistoryData(ab).then(res => {
+      console.log(res[0].All.dates);
+      renderChart(ctx, res[0].All.dates, res[1].All.dates, res[2].All.dates);
+      hideLoading();
+    });
+    
+  });  
 }
 
 const loadCurrentData = async (ab) => {
   try {
     const result = await covidApi.getCurrentDatas(ab); 
-    //console.log(result);
     return result;
   } catch (e) {
     console.error('Error loading current Covid data', e);
@@ -52,11 +85,64 @@ const loadCurrentData = async (ab) => {
   }
 }
 
+const loadHistoryData = async (ab) => {
+  try {
+    const result = await covidApi.getHistoryAllState(ab);
+    return result;
+  } catch (e) {
+    console.error('Error loading history Covid data', e);
+    throw e;
+  }
+}
+
+let covid_chart;
+function renderChart(ctx, cases, recovered, deaths) {
+  if (typeof covid_chart !== 'undefined') {
+    covid_chart.destroy();
+  } 
+  covid_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: cases,
+          fill: true,
+          borderColor: "#FFF",
+          backroundColor: "#FFF",
+          borderWitdth: 1
+        },
+        {
+          label: "Recovered",
+          data: recovered,
+          fill: false,
+          borderColor: "#009688",
+          backroundColor: "#009688",
+          borderWitdth: 1
+        },
+        {
+          label: "Deaths",
+          data: deaths,
+          fill: false,
+          borderColor: "#f44336",
+          backroundColor: "#f44336",
+          borderWitdth: 1
+        }
+      ],
+      labels: null
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    }
+  });
+}
+
 const displayLoading = () => {
   loader.classList.add("display");
   setTimeout(() => {
     loader.classList.remove("display");
-  }, 10000);
+  }, 60000);
 }
 
 const hideLoading = () => {
--- src/services/covidApi.js
+++ src/services/covidApi.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://covid-api.mmediagroup.fr/v1';
 
 const covidApi = {
-  async getCurrentDatas(ab){
+  async getCurrentDatas(ab) {
     const response = await fetch(`${baseUrl}/cases?ab=${ab}`);
     if (response.status !== 200) {
       throw 'Error loading Covid current data';
@@ -9,8 +9,27 @@ const covidApi = {
     
     const d = await response.json();
     return d.All;
-  }
+  },
+
+  async getHistoryAllState(ab) {
+    const urls = [
+      `${baseUrl}/history?ab=${ab}&status=confirmed`,
+      `${baseUrl}/history?ab=${ab}&status=recovered`,
+      `${baseUrl}/history?ab=${ab}&status=deaths`
+    ];
+    const response = Promise.all(
+      urls.map( url => fetch(url).then(res => res.json()) )
+    );
+    return await response;
+  }, 
   
+  async getHistoryRecovered(ab) {
+    // /history?country=Germany&status=recovered
+  }, 
+
+  async getHistoryDeaths(ab) {
+    // /history?country=Germany&status=deaths
+  } 
 
 }
 export default covidApi;
