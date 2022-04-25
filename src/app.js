c3a83a605df491d7e7c58683f4bd0309bc831177
Gáll Zoltán <gallzoltan@gmail.com>
2022-04-25 22:58:45 +0200

    reverse data, error message

--- src/app.js
+++ src/app.js
@@ -11,17 +11,20 @@ let covid_chart;
 window.addEventListener('DOMContentLoaded', () => {   
   registerComponents(); 
   const form = document.getElementById('form'); 
-  const submitButton = document.getElementById('submit');  
+  const submitButton = document.getElementById('submit');
+  const datePicker = document.getElementById('date-input'); 
+  datePicker.max = getToday();  
   
   displayLoading();    
-  renderCurrentDatas('HU'); 
+  renderCurrentDatas('HU', "2022-01-01"); 
 
   form.addEventListener('submit', e => {
     e.preventDefault();   
     const countries = document.getElementById('countries');
+    
     submitButton.disabled = true;
     displayLoading();    
-    renderCurrentDatas(countries.value);
+    renderCurrentDatas(countries.value, datePicker.value);
     submitButton.disabled = false; 
   }); 
 })
@@ -31,11 +34,13 @@ const registerComponents = () => {
   customElements.define("country-list", CountryList);
 }
 
-const renderCurrentDatas = (ab) => {
+const renderCurrentDatas = (ab, beginDate) => {
+  const errorMessage = document.getElementById('error-message');
   const cardConfirmed = document.getElementById('confirmed');
   const cardRecovered = document.getElementById('recovered');
   const cardDeaths= document.getElementById('deaths');
   const ctx = document.getElementById('axes_line_chart').getContext("2d");
+  
   loadCurrentData(ab).then(res => {
     const local = Intl.NumberFormat('hu-HU');
     cardConfirmed.setDatas( { current: local.format(res.confirmed), update: res.updated } );
@@ -43,23 +48,36 @@ const renderCurrentDatas = (ab) => {
     cardDeaths.setDatas( { current: local.format(res.deaths), update: res.updated } );    
  
     loadHistoryData(ab).then(res => {
-      //console.log(res[0].All.dates);
-      const confirmed = filterDatas(res[0].All.dates);
-      const recovered = filterDatas(res[1].All.dates);
-      const deaths = filterDatas(res[2].All.dates);
+      const confirmed = filterDatas(res[0].All.dates, beginDate);
+      const recovered = filterDatas(res[1].All.dates, beginDate);
+      const deaths = filterDatas(res[2].All.dates, beginDate);
       renderChart(ctx, confirmed, recovered, deaths);
       hideLoading();
-    });    
+    }).catch(() => {
+      hideLoading();
+      errorMessage.style.display = 'block';
+      setTimeout(() => errorMessage.style.display = 'none', 2000);
+    }); 
+
+  }).catch(() => {
+    hideLoading();
+    errorMessage.style.display = 'block';
+    setTimeout(() => errorMessage.style.display = 'none', 2000);
   });  
 }
 
-const filterDatas = (datas) => {
-  const from = Date.parse("2022-01-01");
+const filterDatas = (datas, beginDate) => {
+  const from = Date.parse(beginDate);
   const to = Date.parse(new Date());
-  //delete datas["2022-04-24"];
-  //Object.fromEntries(Object.entries(datas).filter(([key]) => key.includes('2022-')));
-
-  return Object.fromEntries(Object.entries(datas).filter(([key]) => (Date.parse(key) > from && Date.parse(key) < to) ));
+  const reversedKeys = Object.keys(datas).reverse();
+  let result = {};
+  reversedKeys.forEach(key => {  
+    if(Date.parse(key) > from && Date.parse(key) < to) {
+      result[key] = datas[key];
+    }       
+  });
+  //return Object.fromEntries(Object.entries(datas).filter(([key]) => (Date.parse(key) > from && Date.parse(key) < to) ));
+  return result;
 }
 
 const loadCurrentData = async (ab) => {
@@ -133,4 +151,12 @@ const displayLoading = () => {
 
 const hideLoading = () => {
   loader.classList.remove("display");
+}
+
+const getToday = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
 }
